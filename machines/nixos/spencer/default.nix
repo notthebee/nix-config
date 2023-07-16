1a2cf7f4e9db5ceae8aaccbaa56dcd05b9856816
Wolfgang <mail@weirdrescue.pw>
2023-07-16 20:39:50 +0000

    Add spencer

---
+++ machines/nixos/spencer/default.nix
@@ -0,0 +1,33 @@
+{ modulesPath, ... }: {
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot.loader.grub.device = "/dev/vda";
+  boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
+  boot.initrd.kernelModules = [ "nvme" ];
+  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+
+  zramSwap.enable = false;
+
+
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 2048;
+  } ];
+
+
+  networking = {
+    hostName = "spencer";
+    nameservers = [ "1.1.1.1" "9.9.9.9" ];
+    defaultGateway = {
+      address = "***REMOVED***";
+      interface = "ens3";
+    };  
+    interfaces = {    
+      ens3.ipv4 = {    
+        addresses = [{      
+          address = "***REMOVED***";      
+          prefixLength = 25;    
+        }];    
+      };
+    };
+  };
+}
