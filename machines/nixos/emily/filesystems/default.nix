db536edb537521aa4e26f50e6d74e082dae55399
Wolfgang <mail@weirdrescue.pw>
2024-03-10 14:42:30 +0100

    Update DuckDNS module

--- machines/nixos/emily/filesystems/default.nix
+++ machines/nixos/emily/filesystems/default.nix
@@ -161,11 +161,14 @@
     enable = true;
     defaults.autodetected = "-a -o on -S on -s (S/../.././02|L/../../6/03) -n standby,q";
     notifications = {
+      wall = {
+        enable = true;
+      };
       mail = {
         enable = true;
         sender = config.email.fromAddress;
         recipient = config.email.toAddress;
-    };
+      };
     };
   };
 
