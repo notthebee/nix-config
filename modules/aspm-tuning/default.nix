ac9780cf0fb0931e3fd415d15165085094425c08
Wolfgang <mail@weirdrescue.pw>
2024-10-30 21:52:31 +0100

    aspm-tuning: reformat

--- modules/aspm-tuning/default.nix
+++ modules/aspm-tuning/default.nix
@@ -1,25 +1,25 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, ... }:
 let
-aspm_tuning = pkgs.writeShellScriptBin "aspm_tuning" (builtins.readFile ./aspm_tuning.sh);
-in {
-  environment.systemPackages = [ 
-  pkgs.pciutils
-  pkgs.bc
-  aspm_tuning 
-  ];
-
-
-systemd.services.aspm_tuning = {
-  description = "Enable ASPM correctly for all supported devices";
-  wantedBy = [ "multi-user.target" ];
-  path = [
-    pkgs.gnused
+  aspm_tuning = pkgs.writeShellScriptBin "aspm_tuning" (builtins.readFile ./aspm_tuning.sh);
+in
+{
+  environment.systemPackages = [
     pkgs.pciutils
     pkgs.bc
+    aspm_tuning
   ];
-  serviceConfig = {
-    ExecStart = "/run/current-system/sw/bin/bash -c '/run/current-system/sw/bin/aspm_tuning'";
-    Type = "oneshot";
+
+  systemd.services.aspm_tuning = {
+    description = "Enable ASPM correctly for all supported devices";
+    wantedBy = [ "multi-user.target" ];
+    path = [
+      pkgs.gnused
+      pkgs.pciutils
+      pkgs.bc
+    ];
+    serviceConfig = {
+      ExecStart = "/run/current-system/sw/bin/bash -c '/run/current-system/sw/bin/aspm_tuning'";
+      Type = "oneshot";
+    };
   };
-};
 }
