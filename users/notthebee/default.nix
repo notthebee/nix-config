3e4e3b5e9ab6517363eddb436177defa38a4b7a0
Wolfgang <mail@weirdrescue.pw>
2024-11-10 14:24:34 +0100

    [users/notthebee] format

--- users/notthebee/default.nix
+++ users/notthebee/default.nix
@@ -1,20 +1,16 @@
-{ config, inputs, pkgs, lib, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
 {
   nix.settings.trusted-users = [ "notthebee" ];
 
   age.secrets.hashedUserPassword = {
     file = "${inputs.secrets}/hashedUserPassword.age";
   };
-
-
-  email = {
-    fromAddress = "moe@notthebe.ee";
-    toAddress = "server_announcements@mailbox.org";
-    smtpServer = "email-smtp.eu-west-1.amazonaws.com";
-    smtpUsername = "AKIAYYXVLL34J7LSXFZF";
-    smtpPasswordPath = config.age.secrets.smtpPassword.path;
-  };
-
 
   users = {
     users = {
@@ -23,9 +19,16 @@
         uid = 1000;
         isNormalUser = true;
         hashedPasswordFile = config.age.secrets.hashedUserPassword.path;
-        extraGroups = [ "wheel" "users" "video" "podman" ];
+        extraGroups = [
+          "wheel"
+          "users"
+          "video"
+          "podman"
+        ];
         group = "notthebee";
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGUGMUo1dRl9xoDlMxQGb8dNSY+6xiEpbZWAu6FAbWw moe@notthebe.ee" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGUGMUo1dRl9xoDlMxQGb8dNSY+6xiEpbZWAu6FAbWw moe@notthebe.ee"
+        ];
       };
     };
     groups = {
