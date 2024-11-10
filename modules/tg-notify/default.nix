904f99c2fae51f87b9f2063d196b5178146626df
Wolfgang <mail@weirdrescue.pw>
2024-11-10 14:38:40 +0100

    [modules/tg-notify] Move agenix secret definitions to the tg-notify module

--- modules/tg-notify/default.nix
+++ modules/tg-notify/default.nix
@@ -1,91 +1,110 @@
-{ config, pkgs, ... }: 
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 let
-notify = pkgs.writeShellScriptBin "notify" 
-''
-#!/bin/bash
-api_key=$(cat ${config.age.secrets.telegramApiKey.path})
-channel_id=$(cat ${config.age.secrets.telegramChannelId.path})
+  notify = pkgs.writeShellScriptBin "notify" ''
+    #!/bin/bash
+    api_key=$(cat ${config.age.secrets.telegramApiKey.path})
+    channel_id=$(cat ${config.age.secrets.telegramChannelId.path})
 
 
-POSITIONAL_ARGS=()
+    POSITIONAL_ARGS=()
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -t)
-      title="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -m)
-      message="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -s)
-      severity="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -*|--*)
-      echo "Unknown option $1"
-      exit 1
-      ;;
-    *)
-      POSITIONAL_ARGS+=("$1") # save positional arg
-      shift # past argument
-      ;;
-  esac
-done
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        -t)
+          title="$2"
+          shift # past argument
+          shift # past value
+          ;;
+        -m)
+          message="$2"
+          shift # past argument
+          shift # past value
+          ;;
+        -s)
+          severity="$2"
+          shift # past argument
+          shift # past value
+          ;;
+        -*|--*)
+          echo "Unknown option $1"
+          exit 1
+          ;;
+        *)
+          POSITIONAL_ARGS+=("$1") # save positional arg
+          shift # past argument
+          ;;
+      esac
+    done
 
-set -- "''${POSITIONAL_ARGS[@]}"
+    set -- "''${POSITIONAL_ARGS[@]}"
 
-declare -a error_messages=(
-"Permanent errors have been detected"
-"UNAVAIL"
-"devices are faulted"
-"DEGRADED"
-"unrecoverable error"
-)
+    declare -a error_messages=(
+    "Permanent errors have been detected"
+    "UNAVAIL"
+    "devices are faulted"
+    "DEGRADED"
+    "unrecoverable error"
+    )
 
-if [ -z "''${message}" ] || [ "''${message}" == " " ]; then
-  message=$(</dev/stdin)
-fi
-
-if [ -z "''${severity}" ] || [ "''${severity}" == " " ]; then
-  emoji="✅"
-  for i in "''${error_messages[@]}"; do
-    if [[ "$message" == *"$i"* ]]; then
-      emoji="❌"
+    if [ -z "''${message}" ] || [ "''${message}" == " " ]; then
+      message=$(</dev/stdin)
     fi
-  done
-else
-  case ''${severity} in
-  "success")
-    emoji="✅"
-    ;;
-  "info")
-    emoji="ℹ️"
-    ;;
-  *)
-    emoji="❌"
-    ;;
-  esac
-  status="Status: ''$severity"
-fi
 
-text="
-$emoji <b>$title on $(${pkgs.systemd}/bin/hostnamectl hostname)</b>
-$status
-$(date)
+    if [ -z "''${severity}" ] || [ "''${severity}" == " " ]; then
+      emoji="✅"
+      for i in "''${error_messages[@]}"; do
+        if [[ "$message" == *"$i"* ]]; then
+          emoji="❌"
+        fi
+      done
+    else
+      case ''${severity} in
+      "success")
+        emoji="✅"
+        ;;
+      "info")
+        emoji="ℹ️"
+        ;;
+      *)
+        emoji="❌"
+        ;;
+      esac
+      status="Status: ''$severity"
+    fi
 
-<code>$message</code>
-"
-/run/current-system/sw/bin/curl -s --data "chat_id=$channel_id" \
-        --data-urlencode "text=$text" \
-        --data-urlencode "parse_mode=HTML" \
-        https://api.telegram.org/$api_key/sendMessage > /dev/null
-'';
-in 
+    text="
+    $emoji <b>$title on $(${pkgs.systemd}/bin/hostnamectl hostname)</b>
+    $status
+    $(date)
+
+    <code>$message</code>
+    "
+    /run/current-system/sw/bin/curl -s --data "chat_id=$channel_id" \
+            --data-urlencode "text=$text" \
+            --data-urlencode "parse_mode=HTML" \
+            https://api.telegram.org/$api_key/sendMessage > /dev/null
+  '';
+in
 {
   environment.systemPackages = [ notify ];
+
+  age.secrets.telegramApiKey = lib.mkDefault {
+    file = "${inputs.secrets}/telegramApiKey.age";
+    mode = "770";
+    owner = "share";
+    group = "share";
+  };
+  age.secrets.telegramChannelId = lib.mkDefault {
+    file = "${inputs.secrets}/telegramChannelId.age";
+    mode = "770";
+    owner = "share";
+    group = "share";
+  };
+
 }
