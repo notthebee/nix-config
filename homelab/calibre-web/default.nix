0195672db65f86c912f5afd938904268f1587035
Wolfgang <mail@weirdrescue.pw>
2024-11-15 06:55:27 +0100

    [homelab/homepage Fix Glances widgets, add a new row

--- homelab/calibre-web/default.nix
+++ homelab/calibre-web/default.nix
@@ -7,31 +7,18 @@
 let
   cfg = config.homelab.services.calibre-web;
   directories = [
-    cfg.mounts.library
+    cfg.mounts.books
     cfg.mounts.config
-    cfg.calibre-web.mounts.config
   ];
 in
 {
-  options.homelab.services.calibre = {
-    enable = lib.mkEnableOption "Self-hosted book server";
-    calibre-web.enable = lib.mkOption {
-      default = true;
-      type = lib.types.bool;
-      description = "Enable Calibre-Web";
-    };
-    calibre-web.mounts.config = lib.mkOption {
+  options.homelab.services.calibre-web = {
+    enable = lib.mkEnableOption "Self-hosted Calibre frontend and server";
+    mounts.config = lib.mkOption {
       default = "${config.homelab.mounts.config}/calibre-web";
       type = lib.types.path;
       description = ''
         Path to Calibre-web configs
-      '';
-    };
-    mounts.config = lib.mkOption {
-      default = "${config.homelab.mounts.config}/calibre";
-      type = lib.types.path;
-      description = ''
-        Path to Calibre configs
       '';
     };
     mounts.library = lib.mkOption {
@@ -46,21 +33,21 @@ in
       default = config.homelab.user;
       type = lib.types.str;
       description = ''
-        User to run Calibre and Calibre-Web as
+        User to run Calibre-Web as
       '';
     };
     group = lib.mkOption {
       default = config.homelab.group;
       type = lib.types.str;
       description = ''
-        User to run Calibre and Calibre-Web as
+        User to run Calibre-Web as
       '';
     };
     timeZone = lib.mkOption {
       default = config.homelab.timeZone;
       type = lib.types.str;
       description = ''
-        Time zone to be used inside the Calibre and Calibre-Web containers
+        Time zone to be used inside the Calibre-Web container
       '';
     };
     baseDomainName = lib.mkOption {
@@ -75,32 +62,6 @@ in
     systemd.tmpfiles.rules = map (x: "d ${x} 0775 ${cfg.user} ${cfg.group} - -") directories;
     virtualisation.oci-containers = {
       containers = {
-        calibre = {
-          image = "lscr.io/linuxserver/calibre:latest";
-          autoStart = true;
-          extraOptions = [
-            "--pull=newer"
-            "-l=homepage.group=Media"
-            "-l=homepage.name=Calibre"
-            "-l=homepage.icon=calibre.svg"
-            "-l=homepage.href=https://calibreserver.${vars.domainName}"
-            "-l=homepage.description=eBook server"
-            "-l=traefik.enable=true"
-            "-l=traefik.http.routers.calibre.rule=Host(`calibreserver.${cfg.baseDomainName}`)"
-            "-l=traefik.http.routers.calibre.service=calibre"
-            "-l=traefik.http.services.calibre.loadbalancer.server.port=8080"
-          ];
-          volumes = [
-            "${cfg.mounts.config}:/config"
-            "${cfg.mounts.library}:/library"
-          ];
-          environment = {
-            TZ = cfg.timeZone;
-            PUID = cfg.user;
-            GUID = cfg.group;
-          };
-        };
-
         calibre-web = {
           image = "lscr.io/linuxserver/calibre-web:latest";
           autoStart = true;
@@ -112,12 +73,12 @@ in
             "-l=homepage.href=https://calibre.${vars.domainName}"
             "-l=homepage.description=eBook management frontend"
             "-l=traefik.enable=true"
-            "-l=traefik.http.routers.calibre-web.rule=Host(`calibre.${cfg.baseDomainName}`)"
-            "-l=traefik.http.routers.calibre-web.service=calibre-web"
-            "-l=traefik.http.services.calibre-web.loadbalancer.server.port=8083"
+            "-l=traefik.http.routers.calibre.rule=Host(`calibre.${cfg.baseDomainName}`)"
+            "-l=traefik.http.routers.calibre.service=calibre"
+            "-l=traefik.http.services.calibre.loadbalancer.server.port=8083"
           ];
           volumes = [
-            "${cfg.calibre-web.mounts.config}:/config"
+            "${cfg.mounts.config}:/config"
             "${cfg.mounts.library}:/library"
           ];
           environment = {
