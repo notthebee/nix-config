d16dd32d12da32e3604b8c5d750bdd69dcbfebac
Wolfgang <mail@weirdrescue.pw>
2024-11-20 10:43:37 +0100

    [machines/darwin] Move HM config from the machine config to flake

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -1,39 +1,10 @@
-{
-  inputs,
-  pkgs,
-  lib,
-  ...
-}:
+{ ... }:
 {
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
     };
-  };
-  imports = [ <home-manager/nix-darwin> ];
-  home-manager = {
-    useGlobalPkgs = false; # makes hm use nixos's pkgs value
-    extraSpecialArgs = {
-      inherit inputs;
-    }; # allows access to flake inputs in hm modules
-    users.notthebee =
-      { config, pkgs, ... }:
-      {
-        home.homeDirectory = lib.mkForce "/Users/notthebee";
-
-        imports = [
-          inputs.nix-index-database.hmModules.nix-index
-          inputs.agenix.homeManagerModules.default
-          ../../users/notthebee/dots.nix
-          ../../users/notthebee/age.nix
-          ../../dots/tmux
-          ../../dots/kitty
-        ];
-      };
-
-    backupFileExtension = "bak";
-    useUserPackages = true;
   };
 
   services.karabiner-elements.enable = true;
