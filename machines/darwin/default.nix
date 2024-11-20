a996e609cb2b81dc64ccde5aca5e5ea7d9906fb5
Wolfgang <mail@weirdrescue.pw>
2024-11-20 19:50:02 +0100

    Patch ghostscript directly instead of pinning nixpkgs

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -1,9 +1,34 @@
-{ ... }:
+{
+  lib,
+  stdenv,
+  fixDarwinDylibNames,
+  ...
+}:
 {
   nixpkgs = {
     config = {
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
+      packageOverrides = pkgs: {
+        ghostscript = pkgs.ghostscript.overrideAttrs (oldAttrs: {
+          dylib_version = lib.versions.major oldAttrs.version;
+          nativeBuildInputs =
+            oldAttrs.nativeBuildInputs
+            ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
+          postInstall = builtins.replaceStrings [
+            ''
+              for file in $out/lib/*.dylib* ; do
+                install_name_tool -id "$file" $file
+              done
+            ''
+          ] [ "" ] oldAttrs.postInstall;
+          preFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
+            for file in $out/bin/{gs,gsc,gsx}; do
+              install_name_tool -change libgs.$dylib_version.dylib $out/lib/libgs.$dylib_version.dylib $file
+            done
+          '';
+        });
+      };
     };
   };
 
