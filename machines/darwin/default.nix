8cb21fed8c468a95021cc98efab74884d13f51c2
Wolfgang <mail@weirdrescue.pw>
2024-12-07 12:17:59 +0100

    Pin Karabiner to 14.13 (https://github.com/LnL7/nix-darwin/issues/1041)

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -5,6 +5,18 @@
       allowUnfree = true;
       allowUnfreePredicate = (_: true);
     };
+    overlays = [
+      (self: super: {
+        karabiner-elements = super.karabiner-elements.overrideAttrs (old: {
+          version = "14.13.0";
+
+          src = super.fetchurl {
+            inherit (old.src) url;
+            hash = "sha256-gmJwoht/Tfm5qMecmq1N6PSAIfWOqsvuHU8VDJY8bLw=";
+          };
+        });
+      })
+    ];
   };
 
   services.karabiner-elements.enable = true;
