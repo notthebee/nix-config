ee201e45c4a7a905ed88f014a257101bf828fe12
Wolfgang <mail@weirdrescue.pw>
2025-01-29 09:23:37 +0100

    Add lidarr

---
+++ homelab/services/arr/lidarr/default.nix
@@ -0,0 +1,51 @@
+{ config, lib, ... }:
+let
+  service = "lidarr";
+  cfg = config.homelab.services.${service};
+  homelab = config.homelab;
+in
+{
+  options.homelab.services.${service} = {
+    enable = lib.mkEnableOption {
+      description = "Enable ${service}";
+    };
+    configDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/${service}";
+    };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "${service}.${homelab.baseDomain}";
+    };
+    homepage.name = lib.mkOption {
+      type = lib.types.str;
+      default = "Lidarr";
+    };
+    homepage.description = lib.mkOption {
+      type = lib.types.str;
+      default = "Music collection manager";
+    };
+    homepage.icon = lib.mkOption {
+      type = lib.types.str;
+      default = "lidarr.svg";
+    };
+    homepage.category = lib.mkOption {
+      type = lib.types.str;
+      default = "Arr";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.${service} = {
+      enable = true;
+      user = homelab.user;
+      group = homelab.group;
+    };
+    services.caddy.virtualHosts."${cfg.url}" = {
+      useACMEHost = homelab.baseDomain;
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8686
+      '';
+    };
+  };
+
+}
