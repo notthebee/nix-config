ad5c8d72b2439f837f055f8a81292adbfa48013a
Wolfgang <mail@weirdrescue.pw>
2025-06-08 12:19:35 +0200

    nftables: Add comments

--- machines/nixos/alison/router/firewall.nix
+++ machines/nixos/alison/router/firewall.nix
@@ -33,17 +33,25 @@ in
           family = "inet";
           content = ''
             chain inbound_world {
+                # Enable ICMPv6 types necessary for DHCPv6
                 ip6 nexthdr icmpv6 icmpv6 type { destination-unreachable, packet-too-big, time-exceeded, parameter-problem, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert, mld-listener-query, nd-router-solicit } accept
+                # Allow port 546/udp for DHCPv6
                 ip6 saddr fe80::/10 iifname ${externalInterface} udp sport 547 udp dport 546 accept
+
+                # Allow Wireguard
                 udp dport ${wgPort} accept
+
                 log prefix "[nftables] Inbound WAN denied: " counter drop
               }
 
               chain inbound_untrusted {
                 icmp type echo-request limit rate 5/second accept
+
+                # Allow DNS and DHCP on untrusted networks (iot, guest)
                 udp dport 53 accept
                 tcp dport 53 accept
                 udp dport 67 accept
+
                 log prefix "[nftables] Untrusted internal denied: " counter drop
               }
 
@@ -67,6 +75,8 @@ in
 
               chain prerouting {
                 type nat hook prerouting priority filter; policy accept;
+
+                # Intercept DNS queries and make sure they get redirected to the router's DNS
                 iifname {"${guest}", "${iot}", "${lan}"} udp dport 53 counter redirect to 53
                 iifname {"${guest}", "${iot}", "${lan}"} tcp dport 53 counter redirect to 53
               }
