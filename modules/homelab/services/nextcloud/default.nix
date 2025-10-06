b72931f50f4e07b698bd633d6a093579c2c6b4cc
Wolfgang <mail@weirdrescue.pw>
2025-10-06 07:59:45 +0200

    Begrudgingly switch back to Nextcloud

---
+++ modules/homelab/services/nextcloud/default.nix
@@ -0,0 +1,113 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  service = "nextcloud";
+  cfg = config.homelab.services.${service};
+  hl = config.homelab;
+in
+{
+  options.homelab.services.${service} = {
+    enable = lib.mkEnableOption {
+      description = "Enable ${service}";
+    };
+    configDir = lib.mkOption {
+      type = lib.types.str;
+      default = "/var/lib/${service}";
+    };
+    url = lib.mkOption {
+      type = lib.types.str;
+      default = "cloud.${hl.baseDomain}";
+    };
+    homepage.name = lib.mkOption {
+      type = lib.types.str;
+      default = "Nextcloud";
+    };
+    homepage.description = lib.mkOption {
+      type = lib.types.str;
+      default = "Enterprise File Storage and Collaboration";
+    };
+    homepage.icon = lib.mkOption {
+      type = lib.types.str;
+      default = "nextcloud.svg";
+    };
+    homepage.category = lib.mkOption {
+      type = lib.types.str;
+      default = "Services";
+    };
+    cloudflared.credentialsFile = lib.mkOption {
+      type = lib.types.str;
+      example = lib.literalExpression ''
+        pkgs.writeText "cloudflare-credentials.json" '''
+        {"AccountTag":"secret"."TunnelSecret":"secret","TunnelID":"secret"}
+        '''
+      '';
+    };
+    cloudflared.tunnelId = lib.mkOption {
+      type = lib.types.str;
+      example = "00000000-0000-0000-0000-000000000000";
+    };
+    admin.username = lib.mkOption {
+      type = lib.types.str;
+      example = "admin";
+    };
+    admin.passwordFile = lib.mkOption {
+      type = lib.types.str;
+      example = lib.literalExpression ''
+        pkgs.writeText "nc-admin-password" '''
+        super-secret-password
+        '''
+      '';
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.nginx.virtualHosts."nix-nextcloud".listen = [
+      {
+        addr = "127.0.0.1";
+        port = 8009;
+      }
+    ];
+    services.cloudflared = {
+      enable = true;
+      tunnels.${cfg.cloudflared.tunnelId} = {
+        credentialsFile = cfg.cloudflared.credentialsFile;
+        default = "http_status:404";
+        ingress."${cfg.url}".service = "http://127.0.0.1:8009";
+      };
+    };
+
+    services.nextcloud = {
+      enable = true;
+      hostName = "nix-nextcloud";
+      package = pkgs.nextcloud31;
+      database.createLocally = true;
+      configureRedis = true;
+      maxUploadSize = "16G";
+      https = true;
+      autoUpdateApps.enable = true;
+      extraAppsEnable = true;
+      extraApps = with config.services.nextcloud.package.packages.apps; {
+        inherit
+          calendar
+          contacts
+          mail
+          notes
+          tasks
+          ;
+
+      };
+
+      config = {
+        overwriteProtocol = "https";
+        defaultPhoneRegion = "DE";
+        dbtype = "pgsql";
+        adminuser = cfg.admin.username;
+        adminpassFile = cfg.admin.passwordFile;
+      };
+    };
+
+  };
+}
